731ebc53235de4167a97cd981dbafc6b4dee0047
Alex <alex.yatseiko@gmail.com>
2019-11-29 04:24:28 +0200

    Single Element Information Added

--- src/ElementItem/ElementItem.js
+++ src/ElementItem/ElementItem.js
@@ -1,42 +1,30 @@
 import React, { Fragment, useState } from 'react'
 import elements from '../PeriodicTableJSON.json'
 import './ElementItem.css';
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 export default function ElementItem() {
-  const [els, setEls] = useState(elements);
+  const [els] = useState(elements);
   let { id } = useParams();
   return (
-    <div>
-      <p>{id}</p>
-      {console.log(els[id-1])}
+    <div className="Element">
+      <Link to="/">Back to Homepage</Link>
+
+      <h1 className="bg-symbol">{els[id-1].symbol}</h1>
+
       <h1>{els[id-1].name}</h1>
+      <h1>Appearance: {els[id-1].appearance}</h1>
+
+      <h1>{els[id-1].discovered_by ? `Discovered By: ${els[id-1].discovered_by}` : ''}</h1>
+      <h1>{els[id-1].named_by ? `Named By: ${els[id-1].named_by}` : ''}</h1>
+      
+      <h1>Atomic mass: {els[id-1].atomic_mass}</h1>
+      <h1>Category: <span className={els[id-1].category}>{els[id-1].category}</span></h1>
+      <h1>Phase: {els[id-1].phase}</h1>
+      
+      <h1>Density: {els[id-1].density}g/cm&#179;</h1>
+      <p>{els[id-1].summary}</p>
+      <a href={els[id-1].source} target="_blank">More</a>
     </div>
   )
 }
-
-
-// export default class ElementItem extends Component {
-//   state = {
-//     element: {}
-//   }
-  
-//   componentDidMount() {
-//     this.setState({
-//       element: elements
-//     })
-//   }
-
-//   render() {
-//     const { element } = this.state
-//     let { id } = useParams();
-
-//     return (
-//       <div>
-//         <h1>React</h1>
-//         <p>{id}</p>
-//         {/* {console.log(this.state)} */}
-//       </div>
-//     )
-//   }
-// }
