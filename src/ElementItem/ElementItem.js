19a08c297628063f1e7c87d21969d422d4b76f74
Alex <alex.yatseiko@gmail.com>
2019-11-30 13:33:50 +0200

    public

--- src/ElementItem/ElementItem.js
+++ src/ElementItem/ElementItem.js
@@ -8,23 +8,33 @@ export default function ElementItem() {
   let { id } = useParams();
   return (
     <div className="Element">
-      <Link to="/">Back to Homepage</Link>
-
       <h1 className="bg-symbol">{els[id-1].symbol}</h1>
+      <p className="bg-number">{id}</p>
+      <Link to="/" className="back">← Home</Link>
+      <div className="elementHeader">
+        <h1 className={`${els[id-1].category} symbol`}>{els[id-1].symbol}</h1>
+        <h1>{els[id-1].name}</h1>
+      </div>
 
-      <h1>{els[id-1].name}</h1>
-      <h1>Appearance: {els[id-1].appearance}</h1>
+      <h3>Category: <span className={`${els[id-1].category} cat`}>{els[id-1].category}</span></h3>
 
-      <h1>{els[id-1].discovered_by ? `Discovered By: ${els[id-1].discovered_by}` : ''}</h1>
-      <h1>{els[id-1].named_by ? `Named By: ${els[id-1].named_by}` : ''}</h1>
+      <p>{els[id-1].summary} <a href={els[id-1].source} target="_blank">Read More →</a></p>
+
+      <h3 className="appearance">{els[id-1].appearance ? `Appearance: ${els[id-1].appearance}` : ''}</h3>
       
-      <h1>Atomic mass: {els[id-1].atomic_mass}</h1>
-      <h1>Category: <span className={els[id-1].category}>{els[id-1].category}</span></h1>
-      <h1>Phase: {els[id-1].phase}</h1>
+      <h3>Atomic mass: {els[id-1].atomic_mass}</h3>
       
-      <h1>Density: {els[id-1].density}g/cm&#179;</h1>
-      <p>{els[id-1].summary}</p>
-      <a href={els[id-1].source} target="_blank">More</a>
+      <h3>Phase: {els[id-1].phase}</h3>
+      
+      <h3>Density: {els[id-1].density} g/cm&#179;</h3>
+
+      <h3>{els[id-1].discovered_by ? `Discovered By: ${els[id-1].discovered_by}` : ''}</h3>
+      <h3>{els[id-1].named_by ? `Named By: ${els[id-1].named_by}` : ''}</h3>
+      
+      <div className="navigation">
+        <Link to={`/element/${id-1}`}>← Previous</Link>
+        <Link to={`/element/${parseInt(id) + 1}`}>Next →</Link>
+      </div>
     </div>
   )
 }
